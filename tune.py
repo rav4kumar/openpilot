fed172ac40ae9ad36254f04da7eda7f01fe01e38
Greg Esmond <gernby2002@yahoo.com>
2019-04-15 15:51:46 -0500

    added dampSteer and reactSteer again

--- tune.py
+++ tune.py
@@ -1,4 +1,7 @@
 from selfdrive.kegman_conf import kegman_conf
+import subprocess
+import os
+BASEDIR = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../"))
 
 letters = { "a":[ "###", "# #", "###", "# #", "# #"], "b":[ "###", "# #", "###", "# #", "###"], "c":[ "###", "#", "#", "#", "###"], "d":[ "##", "# #", "# #", "# #", "##"], "e":[ "###", "#", "###", "#", "###"], "f":[ "###", "#", "###", "#", "#"], "g":[ "###", "# #", "###", "  #", "###"], "h":[ "# #", "# #", "###", "# #", "# #"], "i":[ "###", " #", " #", " #", "###"], "j":[ "###", " #", " #", " #", "##"], "k":[ "# #", "##", "#", "##", "# #"], "l":[ "#", "#", "#", "#", "###"], "m":[ "# #", "###", "###", "# #", "# #"], "n":[ "###", "# #", "# #", "# #", "# #"], "o":[ "###", "# #", "# #", "# #", "###"], "p":[ "###", "# #", "###", "#", "#"], "q":[ "###", "# #", "###", "  #", "  #"], "r":[ "###", "# #", "##", "# #", "# #"], "s":[ "###", "#", "###", "  #", "###"], "t":[ "###", " #", " #", " #", " #"], "u":[ "# #", "# #", "# #", "# #", "###"], "v":[ "# #", "# #", "# #", "# #", " #"], "w":[ "# #", "# #", "# #", "###", "###"], "x":[ "# #", " #", " #", " #", "# #"], "y":[ "# #", "# #", "###", "  #", "###"], "z":[ "###", "  #", " #", "#", "###"], " ":[ " "], "1":[ " #", "##", " #", " #", "###"], "2":[ "###", "  #", "###", "#", "###"], "3":[ "###", "  #", "###", "  #", "###"], "4":[ "#", "#", "# #", "###", "  #"], "5":[ "###", "#", "###", "  #", "###"], "6":[ "###", "#", "###", "# #", "###"], "7":[ "###", "  # ", " #", " #", "#"], "8":[ "###", "# #", "###", "# #", "###"], "9":[ "###", "# #", "###", "  #", "###"], "0":[ "###", "# #", "# #", "# #", "###"], "!":[ " # ", " # ", " # ", "   ", " # "], "?":[ "###", "  #", " ##", "   ", " # "], ".":[ "   ", "   ", "   ", "   ", " # "], "]":[ "   ", "   ", "   ", "  #", " # "], "/":[ "  #", "  #", " # ", "# ", "# "], ":":[ "   ", " # ", "   ", " # ", "   "], "@":[ "###", "# #", "## ", "#  ", "###"], "'":[ " # ", " # ", "   ", "   ", "   "], "#":[ " # ", "###", " # ", "###", " # "], "-":[ "  ", "  ","###","   ","   "] }
 # letters stolen from here: http://www.stuffaboutcode.com/2013/08/raspberry-pi-minecraft-twitter.html
@@ -38,7 +41,13 @@ button_delay = 0.2
 kegman = kegman_conf()
 #kegman.conf['tuneGernby'] = "1"
 #kegman.write_config(kegman.conf)
-param = ["tuneGernby", "reactMPC", "dampMPC", "rateFF", "Kp", "Ki"]
+param = ["tuneGernby", "reactMPC", "dampMPC", "reactSteer", "dampSteer", "rateFF", "Kp", "Ki"]
+
+cmd = '/usr/local/bin/python /data/openpilot/dashboard.py'
+process = subprocess.Popen(cmd, shell=True,
+                           stdout=subprocess.PIPE,
+                           stderr=None,
+                           close_fds=True)
 
 j = 0
 while True:
@@ -52,7 +61,13 @@ while True:
   print "would cause the vehicle to turn sooner."
   print ""
   print ""
-  print "dampMPC is the amount of time that the samples"
+  print "reactSteer is an adjustment to the time projection of steering"
+  print "rate to determine future steering angle.  If the steering is "
+  print "too shaky, decrease this value (may be negative).  If the"
+  print "steering response is too slow, increase this value."
+  print ""
+  print ""
+  print "dampMPC / dampSteer is the amount of time that the samples"
   print "will be projected and averaged to smooth the values"
   print ""
   print ""
@@ -123,11 +138,17 @@ while True:
   if float(kegman.conf['tuneGernby']) != 1 and float(kegman.conf['tuneGernby']) != 0:
     kegman.conf['tuneGernby'] = "1"
 
+  if float(kegman.conf['dampSteer']) < 0 and float(kegman.conf['dampSteer']) != -1:
+    kegman.conf['dampSteer'] = "0"
+
   if float(kegman.conf['dampMPC']) < 0 and float(kegman.conf['dampMPC']) != -1:
     kegman.conf['dampMPC'] = "0"
 
   if float(kegman.conf['dampMPC']) > 0.3:
     kegman.conf['dampMPC'] = "0.3"
+
+  if float(kegman.conf['dampSteer']) > 1.0:
+    kegman.conf['dampSteer'] = "1.0"
 
   if float(kegman.conf['reactMPC']) < -0.99 and float(kegman.conf['reactMPC']) != -1:
     kegman.conf['reactMPC'] = "-0.99"
@@ -137,6 +158,12 @@ while True:
 
   if float(kegman.conf['rateFF']) <= 0.0:
     kegman.conf['rateFF'] = "0.001"
+
+  if float(kegman.conf['reactSteer']) < -0.99 and float(kegman.conf['reactSteer']) != -1:
+    kegman.conf['reactSteer'] = "-0.99"
+
+  if float(kegman.conf['reactSteer']) > 1.0:
+    kegman.conf['reactSteer'] = "1.0"
 
   if float(kegman.conf['Ki']) < 0 and float(kegman.conf['Ki']) != -1:
     kegman.conf['Ki'] = "0"
@@ -149,8 +176,6 @@ while True:
 
   if float(kegman.conf['Kp']) > 3:
     kegman.conf['Kp'] = "3"
-
-
 
 
   if write_json:
