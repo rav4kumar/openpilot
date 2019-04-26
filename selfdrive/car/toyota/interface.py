12dd4d4cda38c88c5027a0b8eaf7acaf9a1ddd2b
Greg Esmond <gernby2002@yahoo.com>
2019-04-26 18:54:31 -0500

    removed restriction of error-cancel

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -288,7 +288,7 @@ class CarInterface(object):
     ret.brakeLights = self.CS.brake_lights
 
     # steering wheel
-    cancellation = np.interp(max(abs(self.avg_error1), self.CS.angle_steers - self.angle_offset_bias), [1.0, 2.0], [self.oscillation_factor, 0.0])
+    cancellation = np.interp(abs(self.avg_error1), [1.0, 2.0], [self.oscillation_factor, 0.0])
     projected_error = float(self.angles_error[(self.frame - self.oscillation_frames) % 500] - self.avg_error1)
     ret.steeringAngle = self.CS.angle_steers + projected_error * cancellation
     ret.steeringRate = self.CS.angle_steers_rate
