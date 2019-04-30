10df4063ad5c3f5fdeadf7f12bc661d2d5ba1f33
Greg Esmond <gernby2002@yahoo.com>
2019-04-30 00:11:41 -0500

    added live Map to dashboard

--- dashboard.py
+++ dashboard.py
@@ -21,8 +21,10 @@ def dashboard_thread(rate=100):
   ipaddress = "127.0.0.1"
   vEgo = 0.0
   live100 = messaging.sub_sock(context, service_list['live100'].port, addr=ipaddress, conflate=False, poller=poller)
+  liveMap = messaging.sub_sock(context, service_list['liveMapData'].port, addr=ipaddress, conflate=False, poller=poller)
+  #gpsNMEA = messaging.sub_sock(context, service_list['gpsNMEA'].port, addr=ipaddress, conflate=True)
 
-  _live100 = None
+  #_live100 = None
 
   frame_count = 0
 
@@ -58,8 +60,14 @@ def dashboard_thread(rate=100):
   tuneSub.setsockopt_string(zmq.SUBSCRIBE, user_id)
   influxFormatString = user_id + ",sources=capnp apply_steer=;noise_feedback=;ff_standard=;ff_rate=;ff_angle=;angle_steers_des=;angle_steers=;dampened_angle_steers_des=;steer_override=;v_ego=;p=;i=;f=;cumLagMs=; "
   kegmanFormatString = user_id + ",sources=kegman dampMPC=;reactMPC=;dampSteer=;reactSteer=;KpV=;KiV=;rateFF=;angleFF=;delaySteer=;oscFactor=;oscPeriod=; "
+  mapFormatString = "location,user=" + user_id + " latitude=;longitude=;altitude=;speed=;bearing=;accuracy=;speedLimitValid=;speedLimit=;curvatureValid=;curvature=;wayId=;distToTurn=;mapValid=;speedAdvisoryValid=;speedAdvisory=;speedAdvisoryValid=;speedAdvisory=;speedLimitAheadValid=;speedLimitAhead=;speedLimitAheadDistance=; "
+  gpsFormatString="gps,user=" + user_id + " "
   influxDataString = ""
   kegmanDataString = ""
+  mapDataString = ""
+
+  lastGPStime = 0
+  lastMaptime = 0
 
   monoTimeOffset = 0
   receiveTime = 0
@@ -72,6 +80,22 @@ def dashboard_thread(rate=100):
         with open('/data/kegman.json', 'w') as f:
           json.dump(config, f, indent=2, sort_keys=True)
           os.chmod("/data/kegman.json", 0o764)
+
+      if socket is liveMap:
+        _liveMap = messaging.drain_sock(socket)
+        for lmap in _liveMap:
+          if vEgo > 0:
+            receiveTime = int((monoTimeOffset + lmap.logMonoTime) * .0000002) * 5
+            if (abs(receiveTime - int(time.time() * 1000)) > 10000):
+              monoTimeOffset = (time.time() * 1000000000) - lmap.logMonoTime
+              receiveTime = int((monoTimeOffset + lmap.logMonoTime) * 0.0000002) * 5
+            lm = lmap.liveMapData
+            lg = lm.lastGps
+            print(lm)
+            mapDataString += ("%f,%f,%f,%f,%f,%f,%d,%f,%d,%f,%f,%f,%d,%d,%f,%d,%f,%d,%f,%f,%d|" %
+                  (lg.latitude ,lg.longitude ,lg.altitude ,lg.speed ,lg.bearing ,lg.accuracy ,lm.speedLimitValid ,lm.speedLimit ,lm.curvatureValid
+                  ,lm.curvature ,lm.wayId ,lm.distToTurn ,lm.mapValid ,lm.speedAdvisoryValid ,lm.speedAdvisory ,lm.speedAdvisoryValid ,lm.speedAdvisory
+                  ,lm.speedLimitAheadValid ,lm.speedLimitAhead , lm.speedLimitAheadDistance , receiveTime))
 
       if socket is live100:
         _live100 = messaging.drain_sock(socket)
@@ -89,6 +113,45 @@ def dashboard_thread(rate=100):
                 l100.live100.steerOverride, vEgo, l100.live100.upSteer, l100.live100.uiSteer, l100.live100.ufSteer, l100.live100.cumLagMs, receiveTime))
 
             frame_count += 1
+
+    #if lastGPStime + 2.0 <= time.time():
+    #  lastGPStime = time.time()
+    #  _gps = messaging.recv_one_or_none(gpsNMEA)
+    #  print(_gps)
+    #if lastMaptime + 2.0 <= time.time():
+    #  lastMaptime = time.time()
+    #  _map = messaging.recv_one_or_none(liveMap)
+
+    '''liveMapData = (
+    speedLimitValid = false,
+    speedLimit = 0,
+    curvatureValid = false,
+    curvature = 0,
+    wayId = 0,
+    lastGps = (
+      flags = 0,
+      latitude = 44.7195573,
+      longitude = -100.8218663,
+      altitude = 10542.853000000001,
+      speed = 0,
+      bearing = 0,
+      accuracy = 4294967.5,
+      timestamp = 1556581592999,
+      source = ublox,
+      vNED = [0, 0, 0],
+      verticalAccuracy = 3750000.2,
+      bearingAccuracy = 180,
+      speedAccuracy = 20.001 ),
+    distToTurn = 0,
+    mapValid = false,
+    speedAdvisoryValid = false,
+    speedAdvisory = 0,
+    speedLimitAheadValid = false,
+    speedLimitAhead = 0,
+    speedLimitAheadDistance = 0 ) )
+    '''
+    #else:
+    #  print(time.time())
 
     if frame_count >= 100:
       if kegman_valid:
@@ -113,16 +176,17 @@ def dashboard_thread(rate=100):
         except:
           kegman_valid = False
 
-      insertString = insertString + influxFormatString + "~" + influxDataString
-
+      insertString = insertString + influxFormatString + "~" + influxDataString + "!"
+      insertString = insertString + mapFormatString + "~" + mapDataString
       steerPush.send_string(insertString)
       print(len(insertString))
       frame_count = 0
       influxDataString = ""
       kegmanDataString = ""
+      mapDataString = ""
       insertString = ""
     else:
-      time.sleep(0.1)
+      time.sleep(0.01)
 
 def main(rate=200):
   dashboard_thread(rate)
