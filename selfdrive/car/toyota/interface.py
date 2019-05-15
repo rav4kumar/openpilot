03382b2cfc2cc3dc60be12e231f721050a44fcb5
Greg Esmond <gernby2002@yahoo.com>
2019-05-15 15:08:06 -0500

    tweaked cancellation

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -299,8 +299,8 @@ class CarInterface(object):
     ret.brakeLights = self.CS.brake_lights
 
     # steering wheel
-    cancellation = np.interp(abs(self.avg_error1), [1.0, 2.0], [self.oscillation_factor, 0.0])
-    projected_error = float(self.angles_error[(self.frame - self.oscillation_frames) % 500] - self.avg_error1)
+    cancellation = np.interp(abs(self.avg_error1), [1.0, 2.0], [self.oscillation_factor, 0.5])
+    projected_error = np.clip(float(self.angles_error[(self.frame - self.oscillation_frames) % 500] - self.avg_error1), -0.5, 0.5)
     self.noise_feedback = projected_error * cancellation
     ret.steeringAngle = self.CS.angle_steers + self.noise_feedback
     #print("%1.1f   %1.1f  %1.1f   %1.2f   %1.1f" % (self.oscillation_frames, self.oscillation_factor, projected_error, cancellation, ret.steeringAngle))
