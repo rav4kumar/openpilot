ccab55dae071f4a8b23064953cee2d7a381d73df
Comma Device <device@comma.ai>
2019-06-09 17:13:31 -0500

    updated Toyota defaults

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -93,8 +93,7 @@ class CarInterface(object):
     ret.rateReactTime = -0.07
     ret.oscillationFactor = 0.0
     ret.centerFactor = 2.0
-    ret.centerFactor = 1.5
-    ret.polyDampTime = 0.1
+    ret.polyDampTime = 0.0
     ret.polyReactTime = 0.1
 
     if candidate == CAR.PRIUS:
