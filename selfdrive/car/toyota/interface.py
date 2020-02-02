861eb23334dca88720cc797646049d692b3d75f7
Brian Czapiga <rys@evool.com>
2020-02-01 23:52:02 -0500

    adjust longitudinal tuning

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -291,15 +291,15 @@ class CarInterface(CarInterfaceBase):
     ret.startAccel = 0.0
 
     if ret.enableGasInterceptor:
-      ret.gasMaxBP = [0., 9., 35]
-      ret.gasMaxV = [0.2, 0.5, 0.7]
-      ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
-      ret.longitudinalTuning.kiV = [0.18, 0.12]
+      ret.gasMaxBP = [0., 9., 55]
+      ret.gasMaxV = [0.3, 0.5, 0.7]
+      ret.longitudinalTuning.kpV = [1.0, 0.75, 0.3]
+      ret.longitudinalTuning.kiV = [0.15, 0.1]
     else:
-      ret.gasMaxBP = [0.]
-      ret.gasMaxV = [0.5]
-      ret.longitudinalTuning.kpV = [3.6, 2.4, 1.5]
-      ret.longitudinalTuning.kiV = [0.54, 0.36]
+      ret.gasMaxBP = [0., 9., 55]
+      ret.gasMaxV = [0.3, 0.5, 0.7]
+      ret.longitudinalTuning.kpV = [2.5, 1.5, 0.325]
+      ret.longitudinalTuning.kiV = [0.3, 0.10]
 
     return ret
 
