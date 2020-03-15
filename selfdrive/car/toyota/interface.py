4596780ce10b7ea6d31576dab46715e726040205
Kumar <36933347+rav4kumar@users.noreply.github.com>
2020-03-15 14:54:17 -0700

    shanes rav4 dynamic gas tune!

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -42,6 +42,7 @@ class CarInterface(CarInterfaceBase):
   @staticmethod
   def compute_gb(accel, speed):
     return float(accel) / 4.0
+    #return float(accel / 3.0 * (0.5 + speed / 60.0)) # 0.5x at 0mph, 1x at 70mph
 
   @staticmethod
   def get_params(candidate, fingerprint=gen_empty_fingerprint(), has_relay=False, car_fw=[]):
@@ -58,18 +59,29 @@ class CarInterface(CarInterfaceBase):
 
     ret.steerActuatorDelay = 0.12  # Default delay, Prius has larger delay
     ret.steerLimitTimer = 0.4
-    
+
     #if ret.enableGasInterceptor:
     #  ret.gasMaxBP = [0., 9., 55]
     #  ret.gasMaxV = [0.2, 0.5, 0.7]
     #  ret.longitudinalTuning.kpV = [0.5, 0.4, 0.3]  # braking tune
     #  ret.longitudinalTuning.kiV = [0.135, 0.1]
     #else:
-    ret.gasMaxBP = [0., 9., 55]
-    ret.gasMaxV = [0.2, 0.5, 0.7]
-    ret.longitudinalTuning.kpV = [0.325, 0.325, 0.325]  # braking tune from rav4h
-    ret.longitudinalTuning.kiV = [0.15, 0.10]
-    
+    #ret.gasMaxBP = [0., 9., 55]
+    #ret.gasMaxV = [0.2, 0.5, 0.7]
+    #ret.longitudinalTuning.kpV = [0.325, 0.325, 0.325]  # braking tune from rav4h
+    #ret.longitudinalTuning.kiV = [0.15, 0.10]
+
+    if ret.enableGasInterceptor:
+      ret.gasMaxBP = [0., 9., 35]
+      ret.gasMaxV = [0.2, 0.5, 0.7]
+      ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
+      ret.longitudinalTuning.kiV = [0.18, 0.12]
+    else:
+      ret.gasMaxBP = [0.]
+      ret.gasMaxV = [0.5]
+      ret.longitudinalTuning.kpV = [3.6, 2.4, 1.5]
+      ret.longitudinalTuning.kiV = [0.54, 0.36]
+
     if candidate not in [CAR.PRIUS, CAR.RAV4, CAR.RAV4H]: # These cars use LQR/INDI
       ret.lateralTuning.init('pid')
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
@@ -120,7 +132,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
       ret.lateralTuning.lqr.l = [0.3233671, 0.3185757]
       ret.lateralTuning.lqr.dcGain = 0.002237852961363602
-      
+
     elif candidate in [CAR.RAV4]:
       stop_and_go = True if (candidate in CAR.RAV4H) else False
       ret.safetyParam = 73
@@ -132,14 +144,19 @@ class CarInterface(CarInterfaceBase):
 
       ret.lateralTuning.lqr.scale = 1500.0
       ret.lateralTuning.lqr.ki = 0.06
-      ret.longitudinalTuning.kpV = [0.1, 0.3, 0.3]  # braking tune
-      ret.longitudinalTuning.kiV = [0.1, 0.1]
+
+      #ret.longitudinalTuning.kpV = [0.1, 0.3, 0.3]  # braking tune
+      #ret.longitudinalTuning.kiV = [0.1, 0.1]
+
       ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
       ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
       ret.lateralTuning.lqr.c = [1., 0.]
       ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
       ret.lateralTuning.lqr.l = [0.3233671, 0.3185757]
       ret.lateralTuning.lqr.dcGain = 0.002237852961363602
+      if ret.enableGasInterceptor:
+        ret.longitudinalTuning.kpV = [(1.2 * 0.9) + (3.6 * 0.1), (0.8 * 0.5) + (2.4 * 0.5), (0.5 * 0.24) + (1.5 * 0.76)]
+        ret.longitudinalTuning.kiV = [(0.18 * 0.9) + (0.54 * 0.1), (0.12 * 0.2125) + (0.36 * 0.7875)]
 
     elif candidate == CAR.COROLLA:
       stop_and_go = False
@@ -210,7 +227,7 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 4700. * CV.LB_TO_KG + STD_CARGO_KG  # 4260 + 4-5 people
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.18], [0.015]]  # community tuning
       ret.lateralTuning.pid.kf = 0.00012  # community tuning
-      
+
     elif candidate in [CAR.HIGHLANDER, CAR.HIGHLANDERH]:
       stop_and_go = True
       ret.safetyParam = 73
