578d44e18516a8dd3f355be8147171a1e8e38458
Kumar <36933347+rav4kumar@users.noreply.github.com>
2020-03-15 22:07:35 -0700

    fix

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -99,16 +99,20 @@ def limit_accel_in_turns(v_ego, angle_steers, a_target, CP):# , angle_later)
   this should avoid accelerating when losing the target in turns
   """
 
-  a_total_max = interp(v_ego, _A_TOTAL_MAX_BP, _A_TOTAL_MAX_V)
-  a_y = v_ego**2 * abs(angle_steers) * CV.DEG_TO_RAD / (CP.steerRatio * CP.wheelbase)
+  #a_total_max = interp(v_ego, _A_TOTAL_MAX_BP, _A_TOTAL_MAX_V)
+  #a_y = v_ego**2 * abs(angle_steers) * CV.DEG_TO_RAD / (CP.steerRatio * CP.wheelbase)
   #a_y2 = v_ego**2 * abs(angle_later) * CV.DEG_TO_RAD / (CP.steerRatio * CP.wheelbase)
-  a_x_allowed = a_total_max - a_y
+  #a_x_allowed = a_total_max - a_y
   #a_x_allowed2 = a_total_max - a_y2
-  a_target[1] = min(a_target[1], a_x_allowed)#, a_x_allowed2)
-  a_target[0] = min(a_target[0], a_target[1])
+  #a_target[1] = min(a_target[1], a_x_allowed)#, a_x_allowed2)
+  #a_target[0] = min(a_target[0], a_target[1])
+  #return a_target
 
-  return a_target
+  a_total_max = interp(v_ego, _A_TOTAL_MAX_BP, _A_TOTAL_MAX_V)
+  a_y = v_ego**2 * angle_steers * CV.DEG_TO_RAD / (CP.steerRatio * CP.wheelbase)
+  a_x_allowed = math.sqrt(max(a_total_max**2 - a_y**2, 0.))
 
+  return [a_target[0], min(a_target[1], a_x_allowed)]
 
 class Planner():
   def __init__(self, CP):
