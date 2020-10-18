2fb7e98d0858c731916bb2177529b2d5af37f5c3
Arne Schwarck <arneschwarck@gmail.com>
2020-10-18 13:39:35 +0200

    alwayson lkas

--- panda/board/safety/safety_toyota.h
+++ panda/board/safety/safety_toyota.h
@@ -87,9 +87,9 @@ static int toyota_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
 
     // enter controls on rising edge of ACC, exit controls on ACC off
     // exit controls on rising edge of gas press
-    if (addr == 0x1D2) {
-      // 5th bit is CRUISE_ACTIVE
-      int cruise_engaged = GET_BYTE(to_push, 0) & 0x20;
+    if (addr == 0x1D3) {
+      // 15th bit is MAIN_ON
+      int cruise_engaged = GET_BYTE(to_push, 1) >> 7;
       if (!cruise_engaged) {
         controls_allowed = 0;
       }
