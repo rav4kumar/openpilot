b3be28b31b33a644522bc99acce8ffc9f91a8967
rav4kumar <36933347+rav4kumar@users.noreply.github.com>
2020-11-09 10:14:27 -0700

    tss2 creep 07f77daf3f6d17f044115d42bda688af13875eb0

--- common/op_params.py
+++ common/op_params.py
@@ -102,7 +102,7 @@ class opParams:
                         'use_lqr': Param(False, bool, 'Enable this to use LQR as your lateral controller over default with any car'),
                         'corollaTSS2_use_indi': Param(False, bool, 'Enable this to use INDI for lat with your Corolla with TSS2'),
                         'standstill_hack': Param(False, bool, 'Some cars support stop and go, you just need to enable this'),
-                        'steer_actuator_delay': Param(0.5, VT.number, 'The steer actuator delay', live=True)
+                        #'steer_actuator_delay': Param(0.5, VT.number, 'The steer actuator delay', live=True)
                         }
 
     self._params_file = '/data/op_params.json'
