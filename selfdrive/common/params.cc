144837f7d196630fee64502408f87c38dbdf8e6f
Prius <36933347+rav4kumar@users.noreply.github.com>
2021-01-02 10:12:15 -0700

    print

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -118,6 +118,7 @@ int Params::write_db_value(const char* key, const char* value, size_t value_size
 
   // Make sure params path exists
   result = ensure_dir_exists(params_path);
+    printf("ensure_dir_exists: %s\n", );
   if (result < 0) {
     goto cleanup;
   }
