4f061f4c1742ccfea1e8df008d549e918ecbd62a
Prius <36933347+rav4kumar@users.noreply.github.com>
2021-01-04 15:21:33 -0700

    update vw

--- selfdrive/car/volkswagen/carstate.py
+++ selfdrive/car/volkswagen/carstate.py
@@ -217,6 +217,29 @@ class CarState(CarStateBase):
     # We use the speed preference for OP.
     self.displayMetricUnits = not pt_cp.vl["Einheiten_1"]["MFA_v_Einheit_02"]
 
+    # Stock FCW is considered active if a warning is displayed to the driver
+    # or the release bit for brake-jerk warning is set. Stock AEB considered
+    # active if the partial braking or target braking release bits are set.
+    # Ref: VW SSP 890253 "Volkswagen Driver Assistance Systems V2", "Front
+    # Assist with Braking: Golf Family" (applies to all MQB)
+    ret.stockFcw = bool(acc_cp.vl["ACC_10"]["AWV2_Freigabe"])
+    ret.stockAeb = any([bool(acc_cp.vl["ACC_10"]["ANB_Teilbremsung_Freigabe"]),
+                        bool(acc_cp.vl["ACC_10"]["ANB_Zielbremsung_Freigabe"])])
+
+    # Consume blind-spot radar info/warning LED states, if available
+    ret.leftBlindspot = any([bool(acc_cp.vl["SWA_01"]["SWA_Infostufe_SWA_li"]),
+                             bool(acc_cp.vl["SWA_01"]["SWA_Warnung_SWA_li"])])
+    ret.rightBlindspot = any([bool(acc_cp.vl["SWA_01"]["SWA_Infostufe_SWA_re"]),
+                             bool(acc_cp.vl["SWA_01"]["SWA_Warnung_SWA_re"])])
+
+    # Consume SWA (Lane Change Assist) relevant info from factory LDW message
+    # to pass along to the blind spot radar controller
+    self.ldw_lane_warning_left = bool(cam_cp.vl["LDW_02"]["LDW_SW_Warnung_links"])
+    self.ldw_lane_warning_right = bool(cam_cp.vl["LDW_02"]["LDW_SW_Warnung_rechts"])
+    self.ldw_side_dlc_tlc = bool(cam_cp.vl["LDW_02"]["LDW_Seite_DLCTLC"])
+    self.ldw_dlc = cam_cp.vl["LDW_02"]["LDW_DLC"]
+    self.ldw_tlc = cam_cp.vl["LDW_02"]["LDW_TLC"]
+
     # Update ACC radar status.
     # FIXME: This is unfinished and not fully correct, need to improve further
     ret.cruiseState.available = bool(pt_cp.vl["GRA_neu"]['Hauptschalter'])
