5f83d4d2a5b4492802c70e1481886a49d546b265
rav4kumar <36933347+rav4kumar@users.noreply.github.com>
2021-01-06 18:34:48 -0700

    revert vw

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -9,6 +9,7 @@ EventName = car.CarEvent.EventName
 class CarInterface(CarInterfaceBase):
   def __init__(self, CP, CarController, CarState):
     super().__init__(CP, CarController, CarState)
+    can_define = CANDefine(DBC[CP.carFingerprint]['pt'])
 
     self.displayMetricUnitsPrev = None
     self.gas_pressed_prev = False
