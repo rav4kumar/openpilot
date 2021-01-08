e8f1f10776901a78695a0a782528fe7b57d2f53c
rav4kumar <36933347+rav4kumar@users.noreply.github.com>
2021-01-08 11:55:51 -0700

    pm

--- selfdrive/car/toyota/carstate.py
+++ selfdrive/car/toyota/carstate.py
@@ -35,8 +35,8 @@ class CarState(CarStateBase):
     self.distance = 0
     self.read_distance_lines = 0
     if not travis:
-      self.pm = messaging.PubMaster(['liveTrafficData'])
-      self.sm = messaging.SubMaster(['liveMapData', 'DragonConf'])#',latControl',])
+      self.pm = messaging.PubMaster(['liveTrafficData', 'DragonConf'])
+      self.sm = messaging.SubMaster(['liveMapData'])#',latControl',])
     # On NO_DSU cars but not TSS2 cars the cp.vl["STEER_TORQUE_SENSOR"]['STEER_ANGLE']
     # is zeroed to where the steering angle is at start.
     # Need to apply an offset as soon as the steering angle measurements are both received
@@ -97,7 +97,7 @@ class CarState(CarStateBase):
       self.read_distance_lines = cp.vl["PCM_CRUISE_SM"]['DISTANCE_LINES']
       msg_df = messaging.new_message('DragonConf')
       msg_df.dpDynamicFollow = max(self.read_distance_lines - 1, 0)
-      self.sm.send('dpDynamicFollow', msg_df)
+      self.pm.send('dpDynamicFollow', msg_df)
 
     if not travis:
       self.sm.update(0)
