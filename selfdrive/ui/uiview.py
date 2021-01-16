542a358ecbd7a4f124cbb54192d8652e755e2fda
rav4kumar <36933347+rav4kumar@users.noreply.github.com>
2021-01-16 14:35:55 -0700

    debug

--- selfdrive/ui/uiview.py
+++ selfdrive/ui/uiview.py
@@ -1,4 +1,5 @@
-mport os
+#!/usr/bin/env python3
+import os
 import time
 import signal
 import subprocess
@@ -7,7 +8,7 @@ from common.basedir import BASEDIR
 
 services = ['controlsState', 'thermal', 'radarState']  # the services needed to be spoofed to start ui offroad
 procs = {'camerad': 'selfdrive/camerad/camerad', 'ui': 'selfdrive/ui/ui',
-             'modeld': 'selfdrive/modeld/modeld', 'calibrationd': 'selfdrive/locationd/calibrationd.py'}
+         'modeld': 'selfdrive/modeld/modeld', 'calibrationd': 'selfdrive/locationd/calibrationd.py'}
 started_procs = [subprocess.Popen(os.path.join(BASEDIR, procs[p]), cwd=os.path.join(BASEDIR, os.path.dirname(procs[p]))) for p in procs]  # start needed processes
 pm = messaging.PubMaster(services)
 
@@ -16,11 +17,10 @@ dat_cs.controlsState.rearViewCam = False  # ui checks for these two messages
 dat_thermal.thermal.started = True
 
 try:
-    while True:
-          pm.send('controlsState', dat_cs)
-              pm.send('thermal', dat_thermal)
-                  pm.send('radarState', dat_radar)
-                      time.sleep(1 / 100)  # continually send, rate doesn't matter
+  while True:
+    pm.send('controlsState', dat_cs)
+    pm.send('thermal', dat_thermal)
+    pm.send('radarState', dat_radar)
+    time.sleep(1 / 100)  # continually send, rate doesn't matter
 except KeyboardInterrupt:
-    [p.send_signal(signal.SIGINT) for p in started_procs]
-
+  [p.send_signal(signal.SIGINT) for p in started_procs]
