911fa7472794e36d00f1da5dee71198418e9ac54
rav4kumar <36933347+rav4kumar@users.noreply.github.com>
2021-01-16 15:27:12 -0700

    defind default

--- selfdrive/controls/lib/dynamic_follow/df_manager.py
+++ selfdrive/controls/lib/dynamic_follow/df_manager.py
@@ -23,7 +23,7 @@ class dfManager:
 
     self.cur_user_profile = self.op_params.get('dynamic_follow').strip().lower()
     if not isinstance(self.cur_user_profile, str) or self.cur_user_profile not in self.df_profiles.to_idx:
-      self.cur_user_profile = self.df_profiles.default  # relaxed
+      self.cur_user_profile = self.df_profiles.relaxed  # relaxed
       self.op_params.put('dynamic_follow', self.df_profiles.to_profile[self.cur_user_profile])
     else:
       self.cur_user_profile = self.df_profiles.to_idx[self.cur_user_profile]
