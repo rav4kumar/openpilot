dc197b84fc054dd5d6c9bde0bd5d92e1f23076fc
rav4kumar <36933347+rav4kumar@users.noreply.github.com>
2021-06-01 08:20:33 -0700

    tune.

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -88,8 +88,10 @@ class CarInterface(CarInterfaceBase):
       tire_stiffness_factor = 0.6371   # hand-tune
       ret.mass = 3115. * CV.LB_TO_KG + STD_CARGO_KG
       if prius_pid:
-        ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.03]]
-        ret.lateralTuning.pid.kf = 0.000007818594
+        ret.steerActuatorDelay = 0.05
+        ret.steerLimitTimer = 0.5
+        ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.18], [0.015]]  # community tuning
+        ret.lateralTuning.pid.kf = 0.00012  # community tuning
       else:
         ret.steerActuatorDelay = 0
         ret.steerLimitTimer = 5
